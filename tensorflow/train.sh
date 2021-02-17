c770e0c2f4446eb267a0bcd473857b221c69c08c
Daheng <dwang8@nd.edu>
2021-02-16 21:42:26 -0500

    Clean dep files

--- tensorflow/train.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-### Make sure packges in requirements.txt are installed
-### If you use virtualenv:
-#source venv/bin/activate
-
-python model_mtie.py --itemlist data/itemlist.txt --behaviorlist data/behaviorlist.txt --output embeddings.mode1 --mode 1 --threads 8
-
-# python model_mtie.py --itemlist data/itemlist.txt --behaviorlist data/behaviorlist.txt --output embeddings.mode2 --mode 2 --threads 8
